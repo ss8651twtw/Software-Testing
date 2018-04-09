bf513bfa0edd5e62f78ba6dc0f2ea213b1d63022
ss8651twtw <ss8651twtw@gmail.com>
2018-04-10 01:43:46 +0800

    fix compare function and test normal boundary for NextDate

--- hw1/nextdate/sample.cc
+++ hw1/nextdate/sample.cc
@@ -1,5 +1,14 @@
 #include "sample.h"
 
+int cmp_date(datetime a, datetime b) {
+  if (a.error == 1 && b.error == 1)
+    return 1;
+  else if (a.error == 0 && b.error == 0)
+    return (a.year == b.year && a.month == b.month && a.day == b.day);
+  else
+    return 0;
+}
+
 datetime NextDate(int year, int month, int day) {
   datetime next;
   return next;
--- hw1/nextdate/sample.h
+++ hw1/nextdate/sample.h
@@ -2,10 +2,10 @@
 
 struct datetime {
   int year, month, day;
-  datetime(int y = 1812, int m = 1, int d = 1) : year(y), month(m), day(d) {}
-  int cmp(const datetime* a, const datetime* b) {
-    return (a->year == b->year && a->month == b->month && a->day == b->day);
-  }
+  int error;
+  datetime(int y = 1900, int m = 1, int d = 1, int e = 0) : year(y), month(m), day(d), error(e) {}
 };
 
 datetime NextDate(int year, int month, int day);
+
+int cmp_date(datetime a, datetime b);
